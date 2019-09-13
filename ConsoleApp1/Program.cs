7fd9edc8712ebfa0232fd778a855da1751196480
ikarian7 <scott.marchant7@gmail.com>
2019-09-13 20:10:46 +0200

    vraag 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,26 +8,41 @@ namespace ConsoleApp1
         {
             int[] highscores = new int[100];
             Random random = new Random();
+            int count = 0;
+            bool isNotUnique;
 
-            for (int i = 0; i < highscores.Length; i++)
+            for (int z = 0; z < 5000; z++)
             {
-                highscores[i] = random.Next(0, 10001);
-            }
-
-            for (int j = 0; j < highscores.Length; j++)
-            {
-                for (int k = 0; k < highscores.Length; k++)
+                isNotUnique = false;
+                for (int i = 0; i < highscores.Length; i++)
                 {
-                    if (k != j && highscores[j] == highscores[k])
+                    highscores[i] = random.Next(0, 10001);
+                }
+
+                for (int j = 0; j < highscores.Length; j++)
+                {
+                    for (int k = 0; k < highscores.Length; k++)
                     {
-                        Console.WriteLine("No");                        
-                    }
-                    else
-                    {
-                        Console.WriteLine("Yes");
+                        if (k != j && highscores[j] == highscores[k])
+                        {
+                            //Console.WriteLine("No");
+                            //count++;
+                            isNotUnique = true;
+                        }
+                        else
+                        {
+                            //Console.WriteLine("Yes");                           
+                        }
                     }
                 }
+                if(isNotUnique)
+                {
+                    count++;
+                }
+                
             }
+            Console.WriteLine(count + " of the runned checks was not unique");
+            Console.WriteLine((count * 100) / 5000 + "% of the runned checks was not unique");
         }
     }
 }
