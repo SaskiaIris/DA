2da25d490a4b4540e5c3154c0a4dd9304dee2f59
ikarian7 <scott.marchant7@gmail.com>
2019-09-13 22:08:04 +0200

    vraag 8 en 9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,51 +1,63 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(/*string[] args*/)
+        static void Main(string[] args)
         {
-            int[] highscores = new int[10000];
+            int[] highscores = new int[100];
             Random random = new Random();
             int count = 0;
-            int randomIndex;
             bool isNotUnique;
-            for (int c = 0; c < 5000; c++)
+            int aantalRuns = 100000;
+            Stopwatch stopwatch = new Stopwatch();
+            //TimeSpan[] runTijd = new TimeSpan[aantalRuns];
+            TimeSpan totaleTijd = 0;
+            TimeSpan gemiddeldeTijd;
+            stopwatch.Start();
+
+            for (int z = 0; z < aantalRuns; z++)
             {
+                Thread.Sleep(1);
                 isNotUnique = false;
                 for (int i = 0; i < highscores.Length; i++)
                 {
-                    highscores[i] = 0;
-                }
-
-                for (int r = 0; r < 100; r++)
-                {
-                    randomIndex = random.Next(0, highscores.Length);
-                    highscores[randomIndex]++;
-                    //randomIndex = 0;
+                    highscores[i] = random.Next(0, 10000);
                 }
 
                 for (int j = 0; j < highscores.Length; j++)
                 {
-                    if (highscores[j] > 1)
+                    for (int k = 0; k < highscores.Length; k++)
                     {
-                        //Console.WriteLine("Yes");
-                        //count++;
-                        isNotUnique = true;
-                    }
-                    else
-                    {
-                        //Console.WriteLine("No");
+                        if (k != j && highscores[j] == highscores[k])
+                        {
+                            //Console.WriteLine("No");
+                            //count++;
+                            isNotUnique = true;
+                        }
+                        else
+                        {
+                            //Console.WriteLine("Yes");                           
+                        }
                     }
                 }
-                if(isNotUnique)
+                if (isNotUnique)
                 {
                     count++;
                 }
+                stopwatch.Stop();
+                //runTijd[z] = stopwatch.Elapsed;
+                totaleTijd += stopwatch.Elapsed;
+                //Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+
             }
-            Console.WriteLine(count + " van de 5000 checks was niet uniek");
-            Console.WriteLine((count * 100) / 5000 + "% van de check was dus niet uniek");
+            gemiddeldeTijd = totaleTijd / aantalRuns;
+            Console.WriteLine("De gemiddelde runtijd was: " + gemiddeldeTijd);
+            //Console.WriteLine(count + " of the runned checks was not unique");
+            //Console.WriteLine((count * 100) / 5000 + "% of the runned checks was not unique");
         }
     }
 }
